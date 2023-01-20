bbc3645a5144feba5178d28bfc0c4f1bc47b2e20
hayashin28 <hayashin.28@gmail.com>
2023-01-21 07:44:03 +0900

    Hello World 1

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,14 @@
 package com.example.hello;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
     
+    @GetMapping("/hello")
+    public String getHell() {
+        // hello.htmlに画面遷移
+        return "hello";
+    }
 }
